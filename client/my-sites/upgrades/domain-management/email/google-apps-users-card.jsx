e11a28d6d974b3b56af85c2ee50dcbd247500ff0
Igor Klimer <igor@klimer.eu>
2016-05-20 03:41:27 +0200

    Domains: Google Apps: Show a notice about logging in to accept ToS

--- client/my-sites/upgrades/domain-management/email/google-apps-users-card.jsx
+++ client/my-sites/upgrades/domain-management/email/google-apps-users-card.jsx
@@ -7,11 +7,17 @@ import React from 'react';
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
+import Notice from 'components/notice';
+import NoticeAction from 'components/notice/notice-action';
 import paths from 'my-sites/upgrades/paths';
+import support from 'lib/url/support';
 import analyticsMixin from 'lib/mixins/analytics';
 import SectionHeader from 'components/section-header';
 import GoogleAppsUserItem from './google-apps-user-item';
 import { getSelectedDomain } from 'lib/domains';
+
+const learnMoreLink = <a href={ support.ADDING_GOOGLE_APPS_TO_YOUR_SITE } target="_blank" />,
+	strong = <strong />;
 
 const GoogleAppsUsers = React.createClass( {
 	mixins: [ analyticsMixin( 'domainManagement', 'googleApps' ) ],
@@ -46,9 +52,62 @@ const GoogleAppsUsers = React.createClass( {
 		this.recordEvent( 'addGoogleAppsUserClick', this.props.selectedDomainName );
 	},
 
+	getGoogleAppsUrl( domain ) {
+		return `https://mail.google.com/a/${ domain }`;
+	},
+
+	pendingTosAcceptanceNotice() {
+		const pendingAccounts = this.props.googleAppsUsers.filter( user => ! user.agreed_to_terms );
+
+		switch( pendingAccounts.length ) {
+			case 0:
+				return null;
+
+			case 1:
+				return this.pendingAccountNotice( pendingAccounts[0] );
+
+			default:
+				return this.pendingAccountsNotice( pendingAccounts );
+		}
+	},
+
+	pendingAccountNotice( { domain, email } ) {
+		return (
+			<Notice
+				status="is-error"
+				showDismiss={ false }
+				key="pending-tos-acceptance-account"
+				text={ this.translate( 'Log in to your {{strong}}%(email)s{{/strong}} Google Apps account to accept Google\'s Terms of Service or else it will get suspended soon. {{learnMoreLink}}Learn more{{/learnMoreLink}}.', { args: { email }, components: { learnMoreLink, strong } } ) }>
+				<NoticeAction href={ this.getGoogleAppsUrl( domain ) } external>
+					{ this.translate( 'Log in' ) }
+				</NoticeAction>
+			</Notice>
+		);
+	},
+
+	pendingAccountsNotice( pendingAccounts ) {
+		return (
+			<Notice
+				status="is-error"
+				showDismiss={ false }
+				key="pending-tos-acceptance-accounts">
+				{ this.translate( 'You need to log in to the following Google Apps accounts and accept Google\'s Terms of Service or else those accounts will get suspended. {{learnMoreLink}}Learn more{{/learnMoreLink}}.', { components: { learnMoreLink } } ) }
+				<ul>{
+					pendingAccounts.map( ( { domain, email, mailbox } ) => {
+						return <li key={ `${ mailbox }${ domain }` }>
+							<strong>{ email }</strong> <a href={ this.getGoogleAppsUrl( domain ) } target="_blank">{ this.translate( 'Log in' ) }</a>
+						</li>;
+					} )
+				}</ul>
+			</Notice>
+		);
+	},
+
 	render() {
 		return (
 			<div>
+				{ this.pendingTosAcceptanceNotice() }
+
 				<SectionHeader
 					count={ this.props.googleAppsUsers.length }
 					label={ this.translate( 'Google Apps Users' ) }>
@@ -63,6 +122,7 @@ const GoogleAppsUsers = React.createClass( {
 						</a>
 					) }
 				</SectionHeader>
+
 				<CompactCard className="google-apps-users-card">
 					<ul className="google-apps-users-card__user-list">
 						{ this.props.googleAppsUsers.map(
