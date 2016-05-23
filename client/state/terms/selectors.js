6df281d182130351e804f808b293cebf7837b82a
Timmy Crawford <timmydcrawford@gmail.com>
2016-05-23 08:09:20 -0700

    Memoize exepnsive term selectors.

--- client/state/terms/selectors.js
+++ client/state/terms/selectors.js
@@ -7,9 +7,58 @@ import values from 'lodash/values';
 /**
  * Internal dependencies
  */
+import TreeConvert from 'lib/tree-convert';
+import createSelector from 'lib/create-selector';
 import {
 	getSerializedTermsQuery
 } from './utils';
+
+/**
+ * Memoized selector returns an array of terms for the taxonomies query
+ * or null if no terms have been received.
+ *
+ * @param  {Object}  state           Global state tree
+ * @param  {Number}  siteId          Site ID
+ * @param  {String}  taxonomy        Taxonomy slug
+ * @param  {String}  serializedQuery Post query object
+ * @return {?Array}                  Posts for the post query
+ */
+const getTermsForSerializedQuery = createSelector(
+	( state, siteId, taxonomy, serializedQuery ) => {
+		const queryResults = get( state.terms.queries, [ siteId, taxonomy, serializedQuery ] );
+		if ( ! queryResults ) {
+			return null;
+		}
+
+		return queryResults.reduce( ( memo, termId ) => {
+			const term = get( state.terms, [ 'items', siteId, taxonomy, termId ] );
+			if ( term ) {
+				memo.push( term );
+			}
+
+			return memo;
+		}, [] );
+	},
+	( state ) => [ state.terms.items ]
+);
+
+/**
+ * Returns an array of terms for the taxonomy query, including all known queried
+ * terms, preserving hierarchy. Returns null if no terms have been received.
+ * Hierarchy is represented by `parent` and `items` properties on each post.
+ *
+ * @param  {Object} state           Global state tree
+ * @param  {Number} siteId          Site ID
+ * @param  {String} taxonomy        Taxonomy slug
+ * @param  {Object} serializedQuery Post query object
+ * @return {?Array}                 Hierarchical terms for the taxonomy query
+ */
+const getTermsHierarchyForQuery = createSelector(
+	( state, siteId, taxonomy, serializedQuery ) => {
+
+	},
+	( state ) => [ state.terms.items ]
+);
 
 /**
  * Returns true if currently requesting terms for the taxonomies query, or false
@@ -38,19 +87,7 @@ export function isRequestingTermsForQuery( state, siteId, taxonomy, query ) {
  */
 export function getTermsForQuery( state, siteId, taxonomy, query ) {
 	const serializedQuery = getSerializedTermsQuery( query );
-	const queryResults = get( state.terms.queries, [ siteId, taxonomy, serializedQuery ] );
-	if ( ! queryResults ) {
-		return null;
-	}
-
-	return queryResults.reduce( ( memo, termId ) => {
-		const term = get( state.terms, [ 'items', siteId, taxonomy, termId ] );
-		if ( term ) {
-			memo.push( term );
-		}
-
-		return memo;
-	}, [] );
+	return getTermsForSerializedQuery( state, siteId, taxonomy, serializedQuery );
 }
 
 /**
