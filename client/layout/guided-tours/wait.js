c0d8c37b3b032e292deecd4977d91cbe7120992b
Leif Singer <git@singer.sh>
2016-05-25 16:32:24 +0200

    don't wait indefinitely

--- client/layout/guided-tours/wait.js
+++ client/layout/guided-tours/wait.js
@@ -1,13 +1,21 @@
+const WAIT_INITIAL = 1; // initial wait in milliseconds
+const WAIT_MULTIPLIER = 2;
+const WAIT_MAX = 10000; // give up waiting when delay has grown to 10 seconds
+
 const wait = ( condition, consequence, delay = 0 ) => {
 	if ( condition() ) {
 		consequence();
 		return;
 	}
 
+	if ( delay >= WAIT_MAX ) {
+		return;
+	}
+
 	window.setTimeout( wait.bind( null,
 		condition,
 		consequence,
-		delay ? delay * 2 : 1
+		delay ? delay * WAIT_MULTIPLIER : WAIT_INITIAL
 	), delay );
 };
 
