643cd3222b9d1a659307f354c006ac817baec70d
retrofox <rdsuarez@gmail.com>
2016-05-25 12:59:30 -0300

    query-site-vouchers: first approach

---
+++ client/components/data/query-site-vouchers/index.jsx
@@ -0,0 +1,67 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+/**
+ * Internal dependencies
+ */
+import { isRequestingSiteVouchers } from 'state/sites/vouchers/selectors';
+import { requestSiteVouchers as requestVouchers } from 'state/sites/vouchers/actions';
+
+class QuerySiteVouchers extends Component {
+
+	constructor( props ) {
+		super( props );
+		this.requestVouchers = this.requestVouchers.bind( this );
+	}
+
+	componentWillMount() {
+		this.requestVouchers();
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( nextProps.requestingSiteVouchers ||
+			! nextProps.siteId ||
+			( this.props.siteId === nextProps.siteId ) ) {
+			return;
+		}
+		this.requestVouchers( nextProps );
+	}
+
+	requestVouchers( props = this.props ) {
+		console.log( `-> props -> `, props );
+		console.log( `-> props.requestingSiteVouchers -> `, props.requestingSiteVouchers );
+		console.log( ' ' );
+		if ( ! props.requestingSiteVouchers && props.siteId ) {
+			props.requestVouchers( props.siteId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+QuerySiteVouchers.propTypes = {
+	siteId: PropTypes.number,
+	requestingVouchers: PropTypes.bool,
+	requestVouchers: PropTypes.func
+};
+
+QuerySiteVouchers.defaultProps = {
+	requestVouchers: () => {}
+};
+
+export default connect(
+	( state, ownProps ) => {
+		return {
+			requestingSiteVouchers: isRequestingSiteVouchers( state, ownProps.siteId )
+		};
+	},
+	dispatch => {
+		return bindActionCreators( { requestVouchers }, dispatch );
+	}
+)( QuerySiteVouchers );
